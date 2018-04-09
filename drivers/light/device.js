970d8f4a918ae58d80e3ac53431a28c43301d423
Bas Jansen <b@sjansen.email>
2018-04-10 00:44:12 +0200

    Show updated values

--- drivers/light/device.js
+++ drivers/light/device.js
@@ -8,6 +8,7 @@ class MyDevice extends Homey.Device {
 	onInit() {
 		EventBus.addEventListener(this.getData().id, (data) =>{
 			console.log(data.target.entity_id + ' got an update!');
+			console.log(data.target.new_state.attributes);
 		});
 	}
 
